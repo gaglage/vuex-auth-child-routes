702767d8a2bfaa6d13f4975ee4dc4141625cad7e
gabrielalvarez <gabrielalvarez@dorna.com>
2018-12-17 11:57:36 +0100

    refactor router

--- src/router.js
+++ src/router.js
@@ -1,28 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import routes from './routes'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [{
-      path: '/',
-      redirect: '/login'
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import( /* webpackChunkName: "login" */ './views/Login.vue')
-    },
-    {
-      path: '/secret',
-      component: () => import( /* webpackChunkName: "login" */ './views/Secret/Secret.vue'),
-      children:[
-        {
-          path: 'notes',
-          component: () => import( /* webpackChunkName: "login" */ './views/Secret/Notes.vue')
-        }
-      ]
-    }
-  ]
+  // routes: routes
+  routes
 })
